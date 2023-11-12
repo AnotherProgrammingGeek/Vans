5f6e80bbc7212737ab926e9d3660f02b29e9008e
NewPythonGeek <memet.murad@mail.ru>
2023-11-12 20:38:44 +0300

    changed the vans page and add a van page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ import{Route, Routes,Link} from "react-router-dom"
 import Home from "./pages/Home/Home"
 import About from "./pages/About/About"
 import Vans from "./pages/Vans/Vans"
+import VanDetail from "./pages/VanDetail/VanDetail"
+
+import "./server"
 
 function App() {
   return (
@@ -19,6 +22,7 @@ function App() {
       <Route path='/' element={<Home />}/>
       <Route path='/about' element={<About />}/>
       <Route path='/vans' element={<Vans />}/>
+      <Route path='/vans/:id' element={<VanDetail />}/>
 
     </Routes>
     </>
--- src/pages/Vans/Vans.jsx
+++ src/pages/Vans/Vans.jsx
@@ -1,8 +1,36 @@
+import React, { useEffect, useState } from "react"
+import {Link} from "react-router-dom"
+import VanDetail from "../VanDetail/VanDetail"
 
 export default function Vans(props){
+  let [vans, setVans] = useState([])
+  useEffect(() =>{
+    fetch("/api/vans")
+      .then(res => res.json())
+      .then(data => setVans(data.vans))
+  },[])
+
+  let vanElements = vans.map(van => (
+      <div key = {van.id} className="van-tile">
+        <Link to={`../vans/${van.id}`}>
+          <img src={van.imageUrl}/>
+          <div className="van-info">
+            <h3>{van.name}</h3>
+            <p>${van.price}<span>/day</span></p>
+          </div>
+          <i className={`van-type ${van.type} selected`}>{van.type}</i>
+        </Link>
+      </div>
+  ))
   return (
-    <div>
-      <span>This page is where I am supposed to put vans</span>
+    <div className="van-list-container">
+      <h1>Explore our van options</h1>
+      
+        <div className="van-list">
+          {vanElements}
+        </div>
+      
+
     </div>
   )
 };
